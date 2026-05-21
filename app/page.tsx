6b420022fc49df80c2644e48ae44fe7eeefc8c63
Random13-sketch <agazadenahid98@gmail.com>
2026-05-22 01:13:50 +0400

    add

--- app/page.tsx
+++ app/page.tsx
@@ -57,7 +57,7 @@ export default function Home() {
     files.forEach(file => formData.append("file", file));
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/analyze", {
+      const response = await fetch("https://cv-ranker-9b6t.onrender.com/analyze", {
         method: "POST",
         body: formData,
       });
